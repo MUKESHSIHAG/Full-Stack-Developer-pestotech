117b2141943185da80f86ba01e279bc9bab52615
Mukesh Sihag <mukeshsihag@192.168.1.8>
2024-07-01 13:00:53 +0530

    task-manager application done.

---
+++ backend/app/models.py
@@ -0,0 +1,7 @@
+from . import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    status = db.Column(db.String(20), nullable=False)
